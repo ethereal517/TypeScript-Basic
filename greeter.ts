7fd8d653f79f5042e6f987dadb8fd9a013a07c15
SmartGuy <smartguy@Andreis-Mac.local>
2016-04-06 05:47:48 +0300

    Basic type usage attached

--- greeter.ts
+++ greeter.ts
@@ -17,3 +17,25 @@ function greeter(person: Person) {
 var user = new Student("Jane", "M.", "User");
 
 document.body.innerHTML = greeter(user);
+
+let list: number[] = [1, 2, 3];
+
+let list2: Array<number> = [1, 2, 3];
+
+// tuple
+
+let x: [string, number];
+
+// enum
+
+enum Color { Red, Green, Blue };
+let c: Color = Color.Green;
+
+// Any
+let notSure: any = 4;
+
+// Type Assertion
+
+let someValue: any = "this is a string";
+
+let strLength: number = (<string>someValue).length;
